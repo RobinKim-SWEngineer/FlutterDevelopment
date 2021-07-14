188374a9e6d21469452c4f3470268c2f4b016555
Robin Kim <hgkim@seznam.cz>
2021-07-14 17:53:37 +0200

    PR : Birthdate input UI

--- lib/Screens/BirthInput/Body.dart
+++ lib/Screens/BirthInput/Body.dart
@@ -7,6 +7,36 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  DateTime _selectedDate = DateTime.now();
+
+  _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now());
+
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+        _showSnackBarMessage(
+            message: 'Your birth date is selected!', context: context);
+      });
+    }
+  }
+
+  ScaffoldFeatureController _showSnackBarMessage(
+      {required String message, required BuildContext context}) {
+    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        message,
+      ),
+      duration: Duration(
+        seconds: 1,
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -56,27 +86,55 @@ class _BodyState extends State<Body> {
               flex: 2,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Calculate',
-                          style: TextStyle(
-                            fontSize: 20,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextButton.icon(
+                        onPressed: () {
+                          _selectDate(context);
+                        },
+                        icon: Icon(Icons.calendar_today_outlined),
+                        style: TextButton.styleFrom(
+                          side: BorderSide(
+                            color: Colors.brown,
                           ),
                         ),
-                        style: TextButton.styleFrom(
-                          primary: Colors.white,
-                          backgroundColor: Colors.deepOrange,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15)))
+                        label: Text(
+                          '$_selectedDate'.split(' ')[0],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 80,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Calculate',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: Colors.deepOrange,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)))),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
