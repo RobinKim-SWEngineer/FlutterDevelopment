256c3f0f9c88199bc3957b9e8330b84b9a884ddf
Robin Kim <hgkim@seznam.cz>
2021-07-29 08:28:59 +0200

    Deletion of commented out part

--- lib/Screens/AuthRegistration/auth_registartion_screen.dart
+++ lib/Screens/AuthRegistration/auth_registartion_screen.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:demo/Screens/AuthRegistration/components/body.dart';
 import 'package:demo/Screens/AuthRegistration/components/body_refactored.dart';
 
-class AuthRegistrationScreen extends StatefulWidget {
-  @override
-  _AuthRegistrationScreen createState() => _AuthRegistrationScreen();
-}
-
-class _AuthRegistrationScreen extends State<AuthRegistrationScreen> {
+class AuthRegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
